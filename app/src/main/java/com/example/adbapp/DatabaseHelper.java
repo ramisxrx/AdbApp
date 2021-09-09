787968793ed3a7f9cf2395ef231b1c78a22966a9
Ramos-develops <ramosdevelops@gmail.com>
2021-09-09 22:29:04 +0300

    defining tables mod_2

--- app/src/main/java/com/example/adbapp/DatabaseHelper.java
+++ app/src/main/java/com/example/adbapp/DatabaseHelper.java
@@ -24,4 +24,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_FIELD_ID = "field_id";
     public static final String COLUMN_PARENT_ID = "parent_id";
     public static final String COLUMN_OBJECT_ID = "object_id";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, SCHEMA);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE " + TABLE_NAMES + " (" + COLUMN_ID +
+                " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, " + COLUMN_NAME +
+                " TEXT);");
+
+        db.execSQL("CREATE TABLE " + TABLE_FIELDS + " (" + COLUMN_ID +
+            " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, " + COLUMN_TYPE_ID +
+            " INTEGER, " + COLUMN_NAME_ID + " INTEGER NOT NULL, FOREIGN KEY (" + COLUMN_NAME_ID + ") REFERENCES "
+            + TABLE_NAMES + "(" + COLUMN_ID + ");");
+
+
+
 }
