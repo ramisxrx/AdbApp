0a9d9066efb0cbbba880e98fef77751c40bcc8ec
Ramos Develops <ramosdevelops@gmail.com>
2021-09-10 10:25:47 +0300

    defining tables mod_3

--- app/src/main/java/com/example/adbapp/DatabaseHelper.java
+++ app/src/main/java/com/example/adbapp/DatabaseHelper.java
@@ -7,18 +7,14 @@ import android.content.Context;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "adb.db"; // название бд
     private static final int SCHEMA = 1; // версия базы данных
-    static final String TABLE_TYPES = "type_clusters"; // название таблицы в бд
     static final String TABLE_NAMES = "name_clusters"; // название таблицы в бд
     static final String TABLE_FIELDS = "field_clusters"; // название таблицы в бд
-    static final String TABLE_TIME = "time_clusters"; // название таблицы в бд
     static final String TABLE_MAIN = "main_clusters"; // название таблицы в бд
     static final String TABLE_OBJECTS = "list_objects"; // название таблицы в бд
     // названия столбцов
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_TYPE = "type_";
-    public static final String COLUMN_TYPE_ID = "type_id";
     public static final String COLUMN_TIME = "time_";
-    public static final String COLUMN_TIME_ID = "time_id";
     public static final String COLUMN_NAME = "name_";
     public static final String COLUMN_NAME_ID = "name_id";
     public static final String COLUMN_FIELD_ID = "field_id";
@@ -32,15 +28,36 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE " + TABLE_NAMES + " (" + COLUMN_ID +
-                " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, " + COLUMN_NAME +
-                " TEXT);");
+        db.execSQL("CREATE TABLE " + TABLE_NAMES +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
+                + COLUMN_NAME + " TEXT);");
 
-        db.execSQL("CREATE TABLE " + TABLE_FIELDS + " (" + COLUMN_ID +
-            " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, " + COLUMN_TYPE_ID +
-            " INTEGER, " + COLUMN_NAME_ID + " INTEGER NOT NULL, FOREIGN KEY (" + COLUMN_NAME_ID + ") REFERENCES "
-            + TABLE_NAMES + "(" + COLUMN_ID + ");");
+        db.execSQL("CREATE TABLE " + TABLE_FIELDS +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
+                + COLUMN_TYPE + " INTEGER, "
+                + COLUMN_NAME_ID + " INTEGER NOT NULL, FOREIGN KEY ("
+                + COLUMN_NAME_ID + ") REFERENCES " + TABLE_NAMES + "(" + COLUMN_ID + "));");
 
+        db.execSQL("CREATE TABLE " + TABLE_OBJECTS +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL);");
 
+        db.execSQL("CREATE TABLE " + TABLE_MAIN +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "
+                + COLUMN_OBJECT_ID + " INTEGER NOT NULL, "
+                + COLUMN_PARENT_ID + " INTEGER, "
+                + COLUMN_FIELD_ID + " INTEGER NOT NULL,"
+                + COLUMN_TIME + " INTEGER, FOREIGN KEY ("
+                + COLUMN_OBJECT_ID + ") REFERENCES " + TABLE_OBJECTS + "(" + COLUMN_ID + "), FOREIGN KEY ("
+                + COLUMN_FIELD_ID + ") REFERENCES " + TABLE_FIELDS + "(" + COLUMN_ID + "));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,  int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAMES);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_FIELDS);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_MAIN);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_OBJECTS);
+        onCreate(db);
+    }
 
 }
