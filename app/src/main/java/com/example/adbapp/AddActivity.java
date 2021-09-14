ab07f7f9c07dd2dba8348de3c0d87aebca195ae9
Ramos Develops <ramosdevelops@gmail.com>
2021-09-14 13:27:34 +0300

    All mod_1

--- app/src/main/java/com/example/adbapp/AddActivity.java
+++ app/src/main/java/com/example/adbapp/AddActivity.java
@@ -7,9 +7,12 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FilterQueryProvider;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -23,7 +26,8 @@ public class AddActivity extends AppCompatActivity {
 
     DatabaseHelper sqlHelper;
     SQLiteDatabase db;
-    Cursor userCursor;
+    Cursor fieldCursor;
+    SimpleCursorAdapter fieldAdapter;
     long recordId=0, objectId=0;
 
     @Override
@@ -51,18 +55,60 @@ public class AddActivity extends AppCompatActivity {
         // если 0, то добавление
         if (recordId > 0) {
             // получаем элемент по id из бд
-            userCursor = db.rawQuery("select object_id, name_ FROM " +
+            fieldCursor = db.rawQuery("select object_id, name_ FROM " +
                     "record_clusters INNER JOIN field_clusters ON record_clusters.field_id=field_clusters._id " +
                     "INNER JOIN name_clusters ON field_clusters.name_id=name_clusters._id " +
                     "WHERE  record_clusters._id=?", new String[]{String.valueOf(recordId)});
-            userCursor.moveToFirst();
-            objectId = userCursor.getInt(0);
-            parentRec.setText(userCursor.getString(1));
-            userCursor.close();
+            fieldCursor.moveToFirst();
+            objectId = fieldCursor.getInt(0);
+            parentRec.setText(fieldCursor.getString(1));
+            fieldCursor.close();
         } else {
 
         }
 
+        // если в текстовом поле есть текст, выполняем фильтрацию
+        // данная проверка нужна при переходе от одной ориентации экрана к другой
+        if(!nameBox.getText().toString().isEmpty())
+            fieldAdapter.getFilter().filter(nameBox.getText().toString());
+
+        // установка слушателя изменения текста
+        nameBox.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) { }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            // при изменении текста выполняем фильтрацию
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                fieldCursor = db.rawQuery("select record_claster._id, parent_id, _type, _name, _time FROM " +
+                        "record_clusters INNER JOIN field_clusters ON record_clusters.field_id=field_clusters._id " +
+                        "INNER JOIN name_clusters ON field_clusters.name_id=name_clusters._id " +
+                        "WHERE _name like ?", new String[]{"%" + s.toString() + "%"});
+
+
+                fieldAdapter.getFilter().filter(s.toString());
+            }
+        });
+
+        // устанавливаем провайдер фильтрации
+        fieldAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+            @Override
+            public Cursor runQuery(CharSequence constraint) {
+
+                if (constraint == null || constraint.length() == 0) {
+
+                    return db.rawQuery("select * from " + DatabaseHelper.TABLE, null);
+                }
+                else {
+                    return db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " +
+                            DatabaseHelper.COLUMN_NAME + " like ?", new String[]{"%" + constraint.toString() + "%"});
+                }
+            }
+        });
+
+
+        fieldList.setAdapter(fieldAdapter);
 
     }
 
--- app/src/main/java/com/example/adbapp/MainActivity.java
+++ app/src/main/java/com/example/adbapp/MainActivity.java
@@ -5,35 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView userList;
+    ListView recordList;
     DatabaseHelper databaseHelper;
     SQLiteDatabase db;
     Cursor userCursor, objectCursor, recordCursor;
     SimpleCursorAdapter userAdapter;
+
+    ArrayList<String> records = new ArrayList<String>();
+    ArrayAdapter<String> recordAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userList = (ListView) findViewById(R.id.list);
-        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
-                intent.putExtra("id", id);
-                startActivity(intent);
-            }
-        });
-
+        recordList = (ListView) findViewById(R.id.list);
         databaseHelper = new DatabaseHelper(getApplicationContext());
     }
 
@@ -43,20 +41,44 @@ public class MainActivity extends AppCompatActivity {
         // открываем подключение
         db = databaseHelper.getReadableDatabase();
 
+        recordAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,records);
+        recordList.setAdapter(recordAdapter);
+
         objectCursor = db.rawQuery("select list_objects.id FROM list_objects", null);
         objectCursor.moveToFirst();
         while(!objectCursor.isAfterLast()) {
             recordCursor = db.rawQuery("select record_claster._id, parent_id, _type, _name, _time FROM " +
                     "record_clusters INNER JOIN field_clusters ON record_clusters.field_id=field_clusters._id " +
-                    "INNER JOIN name_clusters ON field_clusters.name_id=name_clusters._id", new String[]{objectCursor.getString(0)})
+                    "INNER JOIN name_clusters ON field_clusters.name_id=name_clusters._id " +
+                    "WHERE object_id=?", new String[]{objectCursor.getString(0)});
 
             recordCursor.moveToFirst();
             while(!recordCursor.isAfterLast() & recordCursor.getInt(1)!=0){
                 recordCursor.moveToNext();
             }
 
+
             objectCursor.moveToNext();
         }
+
+
+
+
+
+
+        recordList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
 
         //получаем данные из бд в виде курсора
         userCursor = db.rawQuery("select record_clusters._id, name_ FROM " +
