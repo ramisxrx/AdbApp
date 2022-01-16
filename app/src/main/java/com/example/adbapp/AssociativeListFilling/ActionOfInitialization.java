21077ce9dd887db7f54793257350eeab129c13f7
Ramos-develops <ramosdevelops@gmail.com>
2022-01-16 21:25:54 +0300

    AddInterfaces mod_1

---
+++ app/src/main/java/com/example/adbapp/AssociativeListFilling/ActionOfInitialization.java
@@ -0,0 +1,18 @@
+package com.example.adbapp.AssociativeListFilling;
+
+import com.example.adbapp.FillingOfList.AssociativeListFilling;
+import com.example.adbapp.Interfaces.JumpCommand;
+import com.example.adbapp.Threads.HandlerThreadOfFilling;
+
+public class ActionOfInitialization implements JumpCommand {
+    AssociativeListFilling associativeListFilling;
+
+    public ActionOfInitialization(AssociativeListFilling associativeListFilling, HandlerThreadOfFilling handlerThreadOfFilling){
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ app/src/main/java/com/example/adbapp/Interfaces/JumpCommand.java
@@ -0,0 +1,5 @@
+package com.example.adbapp.Interfaces;
+
+public interface JumpCommand {
+    public void execute();
+}
